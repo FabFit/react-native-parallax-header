c0fcb2bc59fb543f7c3655cf8b33d03b98811660
Johnatan Botero <johnatan@jbotero.com>
2022-08-29 20:07:12 -0500

    add missing typos

--- src/parallax-header/index.models.ts
+++ src/parallax-header/index.models.ts
@@ -1,16 +1,16 @@
 import { ReactElement } from 'react';
 import { ImageSourcePropType } from 'react-native';
-
 export interface RenderOverlayParameters {
   scrollPositionY?: any;
   scrollDistance?: number;
   maxHeight?: number;
 }
-
 export interface IParallaxHeaderProps {
+  children?: ReactElement;
   maxHeight?: number;
   minHeight?: number;
   heroImage?: ImageSourcePropType;
+  showsVerticalScrollIndicator?: boolean;
   renderOverlay?: ({
     scrollPositionY,
     scrollDistance,
